16c4ca7e5cd079e694576b1a5fcb6f37a93279ab
airamfuentes <airamfuentes2020@gmail.com>
2026-03-04 02:58:56 +0100

    portfolio airamfuentes

---
+++ src/App.jsx
@@ -0,0 +1,199 @@
+import { useRef, useState, useEffect } from "react";
+import ProfileCard from "./components/ProfileCard/ProfileCard";
+import ShinyText from "./components/ShinyText/ShinyText";
+import BlurText from "./components/BlurText/BlurText";
+import ScrambledText from "./components/ScrambledText/ScrambledText";
+import SplitText from "./components/SplitText/SplitText";
+import Lanyard from "./components/Lanyard/Lanyard";
+import GlassIcons from "./components/GlassIcons/GlassIcons";
+import { listTools, listProyek } from "./data";
+import ChromaGrid from "./components/ChromaGrid/ChromaGrid";
+import ProjectModal from "./components/ProjectModal/ProjectModal"; // <-- IMPORT MODAL
+import Aurora from "./components/Aurora/Aurora";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
+function App() {
+  const aboutRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const [selectedProject, setSelectedProject] = useState(null); // null = modal tertutup
+
+  const handleProjectClick = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProject(null);
+  };
+  // -------------------------
+
+  useEffect(() => {
+    const isReload =
+      performance.getEntriesByType("navigation")[0]?.type === "reload";
+
+    if (isReload) {
+      // Ambil path tanpa hash
+      const baseUrl = window.location.origin + "/";
+      window.location.replace(baseUrl);
+    }
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.2 }
+    );
+
+    if (aboutRef.current) {
+      observer.observe(aboutRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <>
+      <div className="absolute top-0 left-0 w-full h-full -z-10 ">
+        <Aurora
+          colorStops={["#577870", "#1F97A6", "#127B99"]}
+          blend={0.5}
+          amplitude={1.0}
+          speed={0.5}
+        />
+      </div>
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+        <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
+          <div className="animate__animated animate__fadeInUp animate__delay-3s">
+            <div className="flex items-center gap-3 mb-4">
+              </div>
+            <h1 className="text-5xl font-bold mb-6">
+              <ShinyText text="Hola, soy Airam Fuentes" disabled={false} speed={3} className='custom-class' />
+            </h1>
+            <BlurText
+              text="Desarrollador web en proceso enfocado en darle un toque creativo e innovador al mundo digital"
+              delay={150}
+              animateBy="words"
+              direction="top"
+              className=" mb-6"
+            />
+            <div className="flex items-center sm:gap-4 gap-2">
+              <a href="https://github.com/airamfuentes" target="_blank" rel="noreferrer" className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors">
+                <ShinyText text="Explora mis proyectos" disabled={false} speed={3} className="custom-class" />
+              </a>
+            </div>
+
+          </div>
+          <div className="md:ml-auto animate__animated animate__fadeInUp animate__delay-4s">
+            <ProfileCard
+              name="Airam Fuentes"
+              title="Desarrollador Web"
+              handle="airamfuentes"
+              status="Coding..."
+              contactText="Contáctame"
+              avatarUrl="./assets/foto_perfil.png"
+              showUserInfo={true}
+              enableTilt={true}
+              enableMobileTilt={false}
+              onContactClick={() => window.location.href = 'mailto:airamfuentes2020@gmail.com'}
+            />
+          </div>
+        </div>
+        <div className="mt-15 mx-auto w-full max-w-[1600px] rounded-3xl border-[5px] border-white-500/40 shadow-[0_0_30px_rgba(168,85,247,0.4)] bg-gradient-to-br from-[#0a0a0a] via-[#111111] to-[#1a1a1a] p-6" id="about">
+          <div className="flex flex-col md:flex-row items-center justify-between gap-10 pt-0 px-8" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
+            <div className="basis-full md:basis-7/12 pr-0 md:pr-8 border-b md:border-b-0 md:border-r border-white-500/30">
+              <div className="flex-1 text-left">
+                <h2 className="text-3xl md:text-4xl font-bold text-white mb-5">
+                  Sobre mí
+                </h2>
+
+                <BlurText
+                  text="Soy Airam Fuentes, estudiante y apasionado del desarrollo web. Fiel representante de que la innovacioón siempre es la clave y lo creativo siempre se impone"
+                  delay={150}
+                  animateBy="words"
+                  direction="top"
+                  className="text-base md:text-lg leading-relaxed mb-10 text-gray-300"
+                />
+
+
+                <ShinyText
+                  text='El mejor modo de predecir el futuro es inventándotelo... "Alan Kay"'
+                  disabled={false}
+                  speed={3}
+                  className="text-sm md:text-base text-violet-400"
+                />
+              </div>
+            </div>
+            <div className="basis-full md:basis-5/12 pl-0 md:pl-8 overflow-hidden max-w-full flex justify-center ">
+              <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
+            </div>
+          </div>
+
+        </div>
+        <div className="tools mt-32">
+          <h1 className="text-4xl/snug font-bold mb-4" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true" >Herramientas / Tecnologías</h1>
+          <p className="w-2/5 text-base/loose opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">Mi stack principal</p>
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+
+            {listTools.map((tool) => (
+              <div
+                key={tool.id} data-aos="fade-up" data-aos-duration="1000" data-aos-delay={tool.dad} data-aos-once="true"
+                className="flex items-center gap-4 p-4 border border-zinc-700 rounded-xl bg-zinc-900/60 backdrop-blur-md hover:bg-zinc-800/80 transition-all duration-300 group shadow-lg"
+              >
+                <img
+                  src={tool.gambar}
+                  alt="Tools Image"
+                  className="w-16 h-16 object-contain bg-zinc-800 p-2 rounded-lg group-hover:bg-zinc-900 transition-all duration-300"
+                />
+                <div className="flex flex-col overflow-hidden">
+                  <div className="truncate">
+                    <ShinyText
+                      text={tool.nama}
+                      disabled={false}
+                      speed={3}
+                      className="text-lg font-semibold block"
+                    />
+                  </div>
+                  <p className="text-sm text-zinc-400 truncate">{tool.ket}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="proyek mt-32 py-10" id="project" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true"></div>
+        <h1 className="text-center text-4xl font-bold mb-2" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Proyectos</h1>
+        <p className="text-base/loose text-center opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">Algunas ideas y trabajos que desarrolle o sigo en proceso</p>
+        <p className="text-center text-yellow-300 mt-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400" data-aos-once="true">En producción... más proyectos pronto</p>
+        <div className="proyek-box mt-14" >
+
+          <div style={{ height: 'auto', position: 'relative' }} data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400" data-aos-once="true" >
+            <ChromaGrid
+              items={listProyek}
+              onItemClick={handleProjectClick}
+              radius={500}
+              damping={0.45}
+              fadeOut={0.6}
+              ease="power3.out"
+            />
+          </div>
+        </div>
+      </main>
+
+      <ProjectModal
+        isOpen={!!selectedProject}
+        onClose={handleCloseModal}
+        project={selectedProject}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,155 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools13 from "/assets/tools/firebase.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools17 from "/assets/tools/php.png";
+import Tools18 from "/assets/tools/vite.png";
+import Tools19 from "/assets/tools/mysql.png";
+
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Firebase",
+    ket: "Framework",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "PHP",
+    ket: "Language",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "Vite",
+    ket: "Framework",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "MySql",
+    ket: "Framework",
+    dad: "1900",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.jpg";
+import Proyek2 from "/assets/proyek/proyek2.jpg";
+import Proyek3 from "/assets/proyek/proyek3.jpg";
+import Proyek4 from "/assets/proyek/proyek4.jpg";
+import Proyek5 from "/assets/proyek/proyek5.jpg";
+import Proyek6 from "/assets/proyek/proyek6.jpg";
+
+export const listProyek = [
+ // {
+   // id: 1,
+   // image: Proyek1,
+   // title: "Website Portfolio Statamic Docentes",
+   // subtitle: "Simple portfolio website using ",
+   // fullDescription:"A mobile application powered by Artificial Intelligence and Machine Learning, developed to assist in the early detection of Adolescent Idiopathic Scoliosis (AIS). By analyzing patient data and patterns, the app provides predictions that can help doctors and patients take preventive measures. This project demonstrates the role of AI in modern healthcare by improving accuracy, accessibility, and efficiency in medical diagnosis.",
+   // borderColor: "#3B82F6",
+   // gradient: "linear-gradient(145deg, #3B82F6, #000)",
+   // url: "https://github.com/rissss21",
+  //  dad: "100",
+ // },
+  
+];
